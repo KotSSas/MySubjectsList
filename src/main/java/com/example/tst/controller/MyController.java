7eae0ee0c7637e937f286b2cc896738e91878eca
Smug <sashasmug@gmail.com>
2022-05-08 12:37:47 +0200

    try of adding error page

--- src/main/java/com/example/tst/controller/MyController.java
+++ src/main/java/com/example/tst/controller/MyController.java
@@ -15,7 +15,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
-@EnableAutoConfiguration(exclude = {ErrorMvcAutoConfiguration.class})
 @Controller
 public class MyController {
     @Autowired
