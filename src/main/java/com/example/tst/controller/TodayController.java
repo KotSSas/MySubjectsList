d9b5cc42492c01f69a80d64a86caafd57e531928
Smug <sashasmug@gmail.com>
2022-05-09 10:16:59 +0200

    By clicking Subject Managment Sys you will be redirected to current day of the week

---
+++ src/main/java/com/example/tst/controller/TodayController.java
@@ -0,0 +1,39 @@
+package com.example.tst.controller;
+
+import com.example.tst.model.Thu;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import javax.persistence.Column;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Map;
+
+@Controller
+public class TodayController {
+
+    @GetMapping("/today")
+    public String tuesTodayController(Model model) {
+        Date currentTime = new Date();
+        SimpleDateFormat sdf = new SimpleDateFormat("E");
+        String format = sdf.format(currentTime);
+        if (format.equals("Mon")){
+            return "redirect:/blog";
+        }else if (format.equals("Tue")) {
+            return "redirect:/tue";
+        }else if (format.equals("Wed")) {
+            return "redirect:/wed";
+        }else if (format.equals("Thu")) {
+            return "redirect:/thu";
+        }else if (format.equals("Fri")) {
+            return "redirect:/tue";
+        }
+        return "";
+    }
+
+
+}
